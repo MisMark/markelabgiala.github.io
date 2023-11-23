33c9fb9adcf5972594260f15216ef2aa150317d4
Markela Bgiala <markelabgiala@gmail.com>
2023-11-23 17:23:02 +0200

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
 const elements = Array.from(document.querySelectorAll(".image-container img, .image-container video"));
 let currentIndex = 0;
 let lastScrollTime = Date.now();
+let startY = 0;
 const scrollThreshold = 1200; // Set the time threshold (in milliseconds)
+const swipeThreshold = 50; // Set the swipe threshold for touch events
 
 // Shuffle the elements randomly
 function shuffleArray(array) {
@@ -13,7 +15,7 @@ function shuffleArray(array) {
 
 shuffleArray(elements);
 
-document.addEventListener('wheel', function (e) {
+function handleWheel(e) {
     const currentTime = Date.now();
 
     // Check if enough time has passed since the last scroll event
@@ -23,7 +25,7 @@ document.addEventListener('wheel', function (e) {
         if (e.deltaY > 0) {
             // Scrolling down
             if (currentIndex < elements.length - 1) {
-                elements[currentIndex].style.zIndex = 0;// Reset the current element
+                elements[currentIndex].style.zIndex = 0; // Reset the current element
                 currentIndex++;
                 elements[currentIndex].style.zIndex = 100; // Bring the next element to the front
                 elements[currentIndex].style.display = 'block';
@@ -37,9 +39,41 @@ document.addEventListener('wheel', function (e) {
             }
         }
     }
-});
+}
 
+function handleTouchStart(e) {
+    startY = e.touches[0].clientY;
+}
 
+function handleTouchMove(e) {
+    const deltaY = e.touches[0].clientY - startY;
+
+    if (Math.abs(deltaY) >= swipeThreshold) {
+        if (deltaY > 0) {
+            // Swiping down
+            if (currentIndex < elements.length - 1) {
+                elements[currentIndex].style.zIndex = 0; // Reset the current element
+                currentIndex++;
+                elements[currentIndex].style.zIndex = 100; // Bring the next element to the front
+                elements[currentIndex].style.display = 'block';
+            }
+        } else {
+            // Swiping up
+            if (currentIndex > 0) {
+                elements[currentIndex].style.zIndex = 0; // Reset the current element
+                currentIndex--;
+                elements[currentIndex].style.zIndex = 100; // Bring the previous element to the front
+            }
+        }
+
+        // Reset startY for the next swipe
+        startY = e.touches[0].clientY;
+    }
+}
+
+document.addEventListener('wheel', handleWheel);
+document.addEventListener('touchstart', handleTouchStart);
+document.addEventListener('touchmove', handleTouchMove);
 
 
 
